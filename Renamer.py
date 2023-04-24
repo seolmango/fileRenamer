71b9a9d74600902bf95ef13a1ba742147225d10f
Seolmango <seolchaehwan@naver.com>
2023-04-24 17:36:35 +0900

    코드 재작성

--- Renamer.py
+++ Renamer.py
@@ -1,55 +1,57 @@
 import os
 import re
 import ast
+import platform
 
-# 에러 정의
-class NotSetedVariableError(Exception):
-    def __init__(self, message):
-        self.message = message
-    
-    def __str__(self):
-        return self.message
-
-# 함수 정의
-def print_version():
-    print("""\
+def print_version_info():
+    try:
+        os.system("cls")
+        print("""\
 \x1b[1;36m================================
 |<reNamer> - Version 2023/04/24|
 |                              |
 |            made by Seolmango |
 ================================\x1b[1;m""")
-    print()
-    return None
-
-def set_folder_path():
-    print_version()
-    FOLDERPATH = input(f"폴더 경로를 입력하세요(기준은 {os.getcwd()}): ")
-    if FOLDERPATH == "":
-        FOLDERPATH = os.getcwd()
-    FOLDERFILES = os.listdir(FOLDERPATH)
-    if len(FOLDERFILES) == 0:
-        print("폴더에 파일이 없습니다.")
+        print()
+        if platform.system() != "Windows":
+            print("\x1b[1;31m[주의] 이 프로그램은 윈도우 환경을 가정하고 제작된 프로그램입니다. 정상적으로 작동하지 않을 수 있습니다. \x1b[1;m")
+    except Exception as e:
+        print("""\
+\x1b[1;31m================================
+|Error Occured!!               |
+================================\x1b[1;m
+[details] > """+str(e))
+        os.system("pause")
         exit()
-    elif len(FOLDERFILES) <= 20:
-        print(f"폴더에 있는 파일의 수: {len(FOLDERFILES)}")
-        for index, name in enumerate(FOLDERFILES):
-            print(f"{index+1}. {name}")
-    else:
-        print(f"폴더에 있는 파일의 수: {len(FOLDERFILES)}")
-        for index, name in enumerate(FOLDERFILES[:20]):
-            print(f"{index+1}. {name}")
-        print("...")
-        print(f"{len(FOLDERFILES)-20}개의 파일이 더 있습니다.")
-    if input("이 폴더가 맞습니까? (Y/N): ").lower() == "y":
-        os.system("cls")
-        return FOLDERPATH
-    else:
-        os.system("cls")
-        return set_folder_path()
 
-def set_search_pattern(file_path):
-    print_version()
-    files = os.listdir(file_path)
+def set_work_folder():
+    print_version_info()
+    print(f"기준 폴더 : {os.getcwd()}")
+    folder_path = input("작업할 폴더를 입력하세요. > ")
+    if folder_path == "":
+        folder_path = os.getcwd()
+    try:
+        folder_files = os.listdir(folder_path)
+    except FileNotFoundError:
+        if input(f"폴더 '{folder_path}'가 존재하지 않습니다. 다시 입력하시겠습니까? (Y/N) > ").lower() == "y":
+            return set_work_folder()
+        else:
+            os.system("pause")
+            exit()
+    else:
+        print(f"해당 폴더에 존재하는 파일의 수 : {len(folder_files)}")
+        for i in range(0, min(len(folder_files), 20)):
+            print(f"{i+1} | {folder_files[i]}")
+        if len(folder_files) > 20:
+            print(f"...{len(folder_files)-20}개의 파일이 더 존재합니다.")
+        if input("이대로 진행하시겠습니까? (Y/N) > ").lower() == "y":
+            return folder_path
+        else:
+            return set_work_folder()
+
+def set_target_files(folder_path):
+    print_version_info()
+    files = os.listdir(folder_path)
     print("""\
 [검색 패턴 문법]
 1. 기존 파일 이름에서 찾은 변수를 사용할때는 (?P<변수명>패턴)으로 사용합니다.
@@ -58,136 +60,137 @@ def set_search_pattern(file_path):
 - \d{글자수}, \w{글자수}, \s{글자수} : 숫자, 문자, 공백으로 이루어진 글자수만큼의 문자
 - 더 많은 패턴은 https://docs.python.org/ko/3/library/re.html 에서 확인하세요.
 """)
-    PATTERN = input("패턴을 입력하세요: ")
-    PATTERN = re.compile(PATTERN)
-    result = pattern_search(files, PATTERN)
-    if len(result) == 0:
-        print("패턴에 일치하는 파일이 없습니다.")
-        if input("다시 입력하시겠습니까? (Y/N): ").lower() == "y":
-            os.system("cls")
-            return set_search_pattern(file_path)
+    pattern_raw = input("패턴을 입력하세요 > ")
+    try:
+        pattern = re.compile(pattern_raw)
+    except Exception:
+        print("패턴이 올바르지 않습니다. 다시 입력해주세요.")
+        os.system("pause")
+        return set_target_files(folder_path)
+    else:
+        result = []
+        try:
+            for file in files:
+                match_data = re.match(pattern, file)
+                if match_data:
+                    result.append([file, match_data.groupdict()])
+        except Exception:
+            print("에러가 발생했습니다. 다시 입력해주세요.")
+            os.system("pause")
+            return set_target_files(folder_path)
         else:
-            os.system("cls")
-            return set_search_pattern(set_folder_path())
-    elif len(result) <= 20:
-        print(f"패턴에 일치하는 파일의 수: {len(result)}")
-        for index, name in enumerate(result):
-            print(f"{index+1}. {name[0]} | {name[1]}")
-    else:
-        print(f"패턴에 일치하는 파일의 수: {len(result)}")
-        for index, name in enumerate(result[:20]):
-            print(f"{index+1}. {name[0]} | {name[1]}")
-        print("...")
-        print(f"{len(result)-20}개의 파일이 더 있습니다.")
-    if input("이 패턴이 맞습니까? (Y/N): ").lower() == "y":
-        os.system("cls")
-        return result
-    else:
-        os.system("cls")
-        return set_search_pattern(file_path)
+            print(f"검색 결과 : {len(result)}개의 파일이 검색되었습니다.")
+            for i in range(0, min(len(result), 20)):
+                print(f"{i+1} | {result[i][0]}")
+            if len(result) > 20:
+                print(f"...{len(result)-20}개의 파일이 더 존재합니다.")
+            if input("이대로 진행하시겠습니까? (Y/N) > ").lower() == "y":
+                return result
+            else:
+                return set_target_files(folder_path)
 
-def set_new_folder_path():
-    print_version()
-    NEWFOLDERPATH = input(f"새로운 폴더 경로를 입력하세요(기준은 {os.getcwd()}): ")
-    if NEWFOLDERPATH == "":
-        NEWFOLDERPATH = os.getcwd()
-    if os.path.exists(NEWFOLDERPATH):
-        print("이미 존재하는 폴더입니다.")
-        if input("이 폴더에 파일을 이동하시겠습니까? (Y/N): ").lower() == "y":
-            os.system("cls")
-            return NEWFOLDERPATH
+def set_output_folder(folder_path):
+    print_version_info()
+    print(f"기준 폴더 : {os.getcwd()}")
+    output_folder = input(f"출력할 폴더를 입력하세요. (입력하지 않으시면 {folder_path}로 가정합니다.)> ")
+    if output_folder == "" or output_folder == folder_path:
+        return folder_path
+    else:
+        if os.path.exists(output_folder):
+            if input(f"폴더 {output_folder}로 파일을 이동하시겠습니까? (Y/N) > ").lower() == "y":
+                return output_folder
+            else:
+                return set_output_folder(folder_path)
         else:
-            return set_new_folder_path()
-    else:
-        os.mkdir(NEWFOLDERPATH)
-        os.system("cls")
-        return NEWFOLDERPATH
+            if input("해당 폴더는 존재하지 않습니다. 해당 폴더를 생성하고 진행하시겠습니까? (Y/N) > ") == "y":
+                os.mkdir(output_folder)
+                return output_folder
+            else:
+                return set_output_folder(folder_path)
 
-def set_rename_pattern(files):
-    print_version()
+def set_output_pattern(result, before_path, after_path):
+    print_version_info()
     print("""\
 [이름 설정 패턴 문법]
 1. 기존 파일 이름에서 찾은 변수를 사용할때는 {변수명}으로 사용합니다.
-2. <(파이썬 코드)>는 그 자리에 그 코드의 실행 결과를 넣습니다. 이 코드 안에도 변수를 넣을 수 있습니다.
+2. <(파이썬 코드)>는 그 자리에 그 코드의 실행 결과를 넣습니다. 이 코드 안에도 {변수}를 넣을 수 있습니다.
 - Counter는 1부터 시작하는 특수한 변수입니다. 변수 Counter는 파일의 순서를 만들 때 사용가능합니다.
 """)
-    NEWPATTERN = input("새로운 패턴을 입력하세요: ")
+    pattern_raw = input("패턴을 입력하세요 > ")
     new_preview = []
-    for index, file in enumerate(files):
-        try:
-            new_preview.append([file, rename_pattern(NEWPATTERN, file[1], index,file[0])])
-        except NotSetedVariableError as e:
-            print(e)
-    if len(new_preview) == 0:
-        print("패턴에 일치하는 파일이 없습니다.")
-        if input("다시 입력하시겠습니까? (Y/N): ").lower() == "y":
-            os.system("cls")
-            return set_rename_pattern(files, NEWPATTERN)
-        else:
-            exit()
-    elif len(new_preview) <= 20:
-        print(f"패턴에 일치하는 파일의 수: {len(new_preview)}")
-        for index, name in enumerate(new_preview):
-            print(f"{index+1}. {name[0]} => {name[1]}")
-    else:
-        print(f"패턴에 일치하는 파일의 수: {len(new_preview)}")
-        for index, name in enumerate(new_preview[:20]):
-            print(f"{index+1}. {name[0]} => {name[1]}")
-        print("...")
-        print(f"{len(new_preview)-20}개의 파일이 더 있습니다.")
-    print("\033[31m" + "주의! 한번 실행된 작업은 되돌릴 수 없습니다" + "\033[0m")
-    if input("이 패턴이 맞습니까? (Y/N): ").lower() == "y":
-        os.system("cls")
+    for index, file in enumerate(result):
+        new_name = ""
+        counter = index+1
+        i = 0
+        while i < len(pattern_raw):
+            if pattern_raw[i] == "{":
+                j = i+1
+                while pattern_raw[j] != "}":
+                    j += 1
+                if pattern_raw[i+1:j] not in file[1]:
+                    print(f"패턴에 존재하지 않는 변수 {pattern_raw[i+1:j]}가 있습니다.")
+                    os.system("pause")
+                    return set_output_pattern(result, before_path, after_path)
+                new_name += str(file[1][pattern_raw[i+1:j]])
+                i = j
+            elif pattern_raw[i] == "<":
+                j = i+1
+                while pattern_raw[j] != ">":
+                    j += 1
+                code = pattern_raw[i+1:j]
+                for i in file[1].keys():
+                    code = code.replace("{"+i+"}", str(file[1][i]))
+                code = code.replace("{Counter}", str(counter))
+                try:
+                    code = ast.literal_eval(code)
+                except Exception:
+                    print(f"파이썬 코드 {code}가 올바르지 않습니다.")
+                    os.system("pause")
+                    return set_output_pattern(result, before_path, after_path)
+                new_name += str(code)
+                i = j
+            else:
+                new_name += pattern_raw[i]
+            i += 1
+        new_preview.append([file[0], new_name])
+    print("파일 이름 변경 미리보기")
+    for i in range(0, min(len(new_preview), 20)):
+        print(f"{i+1} | {new_preview[i][0]} -> {new_preview[i][1]}")
+    if len(new_preview) > 20:
+        print(f"...{len(new_preview)-20}개의 파일이 더 존재합니다.")
+    if input("이대로 진행하시겠습니까? (Y/N) > ").lower() == "y":
+        for file in new_preview:
+            os.rename(os.path.join(before_path, file[0]), os.path.join(after_path, file[1]))
         return new_preview
     else:
-        os.system("cls")
-        return set_rename_pattern(files)
-    
-def pattern_search(files, pattern):
-    result = []
-    for file in files:
-        match = re.match(pattern, file)
-        if match:
-            result.append([file,match.groupdict()])
-    return result
+        return set_output_pattern(result, before_path, after_path)
 
-def rename_pattern(pattern, vars, index,name):
-    vars["Counter"] = index+1
-    new_word = ""
-    if pattern == "":
-        return name
-    i = 0
-    while i < len(pattern):
-        if pattern[i] == "{":
-            j = i+1
-            while pattern[j] != "}":
-                j += 1
-            if pattern[i+1:j] not in vars:
-                raise NotSetedVariableError(f"기존 파일 이름에서 {pattern[i+1:j]}을(를) 찾을 수 없습니다.")
-            new_word += str(vars[pattern[i+1:j]])
-            i = j
-        elif pattern[i] == "<":
-            j = i+1
-            while pattern[j] != ">":
-                j += 1
-            code = pattern[i+1:j]
-            for i in vars.keys():
-                code = code.replace(f"{i}", str(vars[i]))
-            code = code.replace(" ", "")
-            code = ast.literal_eval(code)
-            new_word += str(code)
-            i = j
+def main():
+    folder_path = set_work_folder()
+    target_files = set_target_files(folder_path)
+    output_folder = set_output_folder(folder_path)
+    data = set_output_pattern(target_files, folder_path, output_folder)
+    print_version_info()
+    print("지금이 작업을 복구할 수 있는 마지막 기회입니다. 다시 한번 작업이 잘 진행되었는지 확인해주세요.")
+    if input("작업을 복구하시겠습니까? (Y/N) > ").lower() == "y":
+        try:
+            for file in data:
+                os.rename(os.path.join(output_folder, file[1]), os.path.join(folder_path, file[0]))
+        except Exception:
+            print("작업 복구에 실패했습니다.")
+            os.system("pause")
         else:
-            new_word += pattern[i]
-        i += 1
-    return new_word
+            print("작업이 복구되었습니다.")
+    else:
+        print("작업이 완료되었습니다.")
+    if input("다시 시작하시겠습니까? (Y/N) > ").lower() == "y":
+        return 1
+    else:
+        return 0
 
 if __name__ == "__main__":
-    FOLDERPATH = set_folder_path()
-    result = set_search_pattern(FOLDERPATH)
-    NEWFOLDERPATH = set_new_folder_path()
-    new_preview = set_rename_pattern(result)
-    for old, new in new_preview:
-        os.rename(os.path.join(FOLDERPATH, old[0]), os.path.join(NEWFOLDERPATH, new))
-    print("작업이 완료되었습니다.")
+    while main() == 1:
+        pass
     os.system("pause")
+    exit()
+    
